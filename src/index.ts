81940788686a98abb5435e54a1512c59b1407cf1
v_kedan <v_kedan@tencent.com>
2020-08-04 10:47:10 +0800

    v0.0.3 Delete redundant code

--- src/index.ts
+++ src/index.ts
@@ -11,28 +11,17 @@ export interface ExcelSheet {
   rows: ExcelRow[];
 }
 
-export interface ExcelOutOptions {
-  filename: string;
-}
 
 export interface ExcelOptions {
-  out: ExcelOutOptions;
+
 }
 
 export class Excel {
 
   constructor(public sheets: ExcelSheet[], public options: Partial<ExcelOptions>) { }
 
-  getOptions(): ExcelOptions {
-    if (!this.options.out) {
-      this.options.out = {
-        filename: new Date().getTime() + '.dg.xlsx'
-      }
-    }
-    return this.options as ExcelOptions;
-  }
 
   toXlsx() {
-    return new ExcelXlsx(this.sheets, this.getOptions());
+    return new ExcelXlsx(this.sheets, {});
   }
 }
